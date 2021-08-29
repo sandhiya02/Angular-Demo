947f67589648a61b8d0d947d44869c41435205c8
unknown <83603388+Anu421@users.noreply.github.com>
2021-08-29 15:54:54 +0530

    property commit

---
+++ angular1/src/app/property/property.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property',
+  templateUrl: './property.component.html',
+  styleUrls: ['./property.component.css']
+})
+export class PropertyComponent implements OnInit {
+
+  Isshow=true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
